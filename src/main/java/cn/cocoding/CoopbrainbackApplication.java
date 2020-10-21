48a8e30c1d545970128d0417c3dcdbc3b366bd6e
wuhuzi <wuhuzi@163.com>
2020-10-21 22:04:07 +0800

    coopbrainback architecture

---
+++ src/main/java/cn/cocoding/CoopbrainbackApplication.java
@@ -0,0 +1,13 @@
+package cn.cocoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopbrainbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoopbrainbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/cocoding/CoopbrainbackApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cocoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopbrainbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
